cc9c23f5ab37aeefbf3756279d328199de563a6a
Valera <valeratrades@gmail.com>
2026-02-13 18:15:24 +0000

    move: split into a proper workspace

--- robot_master/src/lib.rs
+++ robot_master/src/lib.rs
@@ -1,7 +1,4 @@
 pub mod app;
-#[cfg(feature = "ssr")]
-pub mod config;
-pub mod game;
 
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
@@ -9,5 +6,5 @@ pub fn hydrate() {
 	console_error_panic_hook::set_once();
 	leptos::mount::hydrate_body(app::App);
 
-	game::create_app().run();
+	robot_master_game::create_app().run();
 }
