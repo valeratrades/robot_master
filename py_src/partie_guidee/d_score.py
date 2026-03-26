e386748794d263d17f3e7e355ee1b3bc8c36b2db
Valera <valeratrades@gmail.com>
2026-03-26 20:46:04 +0000

    chore: type all functions

--- py_src/partie_guidee/d_score.py
+++ py_src/partie_guidee/d_score.py
@@ -6,24 +6,31 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from partie_guidee.b_gestionCartes import *
+from typeguard import typechecked
 
-def init_dico_cartes(dico_options={'maxC':5}):
+
+@typechecked
+def init_dico_cartes(dico_options: dict[str, int] = {'maxC':5}) -> dict[int, int]:
 	"""la fonction init_dico_cartes crée un dictionnaire dont les clés sont des int correspondants aux valeurs possibles des cartes, et les valeurs sont initialisées à 0. maxC est une variables optionnelle donnant la valeur maximal d'une carte"""
 	pass
 
 
-def colonne_to_dico (plateau,joueuse_active,i,dico_options={'maxC':5}):
-	"""La fonction colonne_to_dico prend un plateau, deux entiers, un dictionnaire d'options et retourne un dictionnaire. Si joueuse_active est impaire, on regarde la i ème ligne de plateau (sinon la i ème colonne). On regarde les cartes présente dans cette ligne (ou colonne) et on note le nombre d’occurrences de chaque cartes dans le dictionnaire retourné. Attention le plateau peut contenir des 'None'."""
+@typechecked
+def colonne_to_dico(plateau: Plateau, joueuse_active: int, i: int, dico_options: dict[str, int] = {'maxC':5}) -> dict[int, int]:
+	"""La fonction colonne_to_dico prend un plateau, deux entiers, un dictionnaire d'options et retourne un dictionnaire. Si joueuse_active est impaire, on regarde la i ème ligne de plateau (sinon la i ème colonne). On regarde les cartes présente dans cette ligne (ou colonne) et on note le nombre d'occurrences de chaque cartes dans le dictionnaire retourné. Attention le plateau peut contenir des 'None'."""
 	pass
 
-def score_ligne (dico_ligne):
-	"""Étant donné une ligne (ou une colonne) sous la forme d'un dictionnaire qui recense le nombre d’occurrences de chaque carte, compter le score de la ligne en accord avec les règles du jeux Robot Master"""
+@typechecked
+def score_ligne(dico_ligne: dict[int, int]) -> int:
+	"""Étant donné une ligne (ou une colonne) sous la forme d'un dictionnaire qui recense le nombre d'occurrences de chaque carte, compter le score de la ligne en accord avec les règles du jeux Robot Master"""
 	pass
 
-def score_joueuse(plateau,joueuse_active,dico_options={'maxC':5}):
+@typechecked
+def score_joueuse(plateau: Plateau, joueuse_active: int, dico_options: dict[str, int] = {'maxC':5}) -> tuple[int, int]:
 	"""la fonction score_joueuse retourne un tuple d'entiers. Si joueuse_active est paire, on regarde le score des colone, sinon des ligne. On retourne le score ainsi que l'indice de la colonne (ou la ligne) qui réalise ce score."""
 	pass
 
-def victoire(plateau,dico_options={'maxC':5}):
+@typechecked
+def victoire(plateau: Plateau, dico_options: dict[str, int] = {'maxC':5}) -> tuple[int, int, int, int]:
 	"""La fonction victoire retourne un tuple d'entiers contenant : le score de la joueuse, l'indice de la colonne correspondante, le score du joueur, l'indice de la ligne correspondante."""
 	pass
--- py_src/partie_guidee/z_variablesDeTest.py
+++ py_src/partie_guidee/z_variablesDeTest.py
@@ -2,14 +2,17 @@ from collections import Counter #pour comparer peu importe l'ordre
 import pytest
 
 
-def new_plateau_test():
-	return [[None,None,1,1,0],[None,2,None,3,None],[4,None,None,None,None],[None,2,None,None,0],[4,4,4,0,0]]
-plateau_test=new_plateau_test()
+Plateau = list[list[int | None]]
 
-def new_small_plateau_test():
+
+def new_plateau_test() -> Plateau:
+	return [[None,None,1,1,0],[None,2,None,3,None],[4,None,None,None,None],[None,2,None,None,0],[4,4,4,0,0]]
+plateau_test: Plateau = new_plateau_test()
+
+def new_small_plateau_test() -> Plateau:
 	return ([[None,1,2],[3,None,None],[4,None,None]])
 
-small_plateau_test= new_small_plateau_test()
+small_plateau_test: Plateau = new_small_plateau_test()
 
 # Pour afficher les plateaux de test:
 # from a_plateau import *
