9601c60c8b4d719dc81d78e90660face83721cd6
Valera <valeratrades@gmail.com>
2026-03-27 06:07:42 +0000

    chore: idk if I understood assignment correctly, add prof ai stub + cleanup

--- py_src/IA/__main__.py
+++ py_src/IA/__main__.py
@@ -10,17 +10,21 @@ from tui_core import tui
 
 
 def main() -> None:
-	match ("-m" in sys.argv, "-r" in sys.argv, "-g" in sys.argv, "-a" in sys.argv):
-		case (True, False, False, False):
+	match ("-m" in sys.argv, "-r" in sys.argv, "-g" in sys.argv, "-a" in sys.argv, "-i" in sys.argv, "-p" in sys.argv):
+		case (True, False, False, False, False, False):
 			mode = "m"
-		case (False, True, False, False):
+		case (False, True, False, False, False, False):
 			mode = "r"
-		case (False, False, True, False):
+		case (False, False, True, False, False, False):
 			mode = "g"
-		case (False, False, False, True):
+		case (False, False, False, True, False, False):
 			mode = "a"
+		case (False, False, False, False, True, False):
+			mode = "i"
+		case (False, False, False, False, False, True):
+			mode = "p"
 		case _:
-			print("usage: python -m IA (-m | -r | -g | -a)", file=sys.stderr)
+			print("usage: python -m IA (-m | -r | -g | -a | -i | -p)", file=sys.stderr)
 			sys.exit(1)
 	tui(jeux, mode)
 
